de7fd1ec2b89ccfa32b70850205fda9bbf3769c5
Андрей Левченков <lev_a_a@mail.ru>
2024-07-18 19:14:33 +0200

    z40

---
+++ Zadanie40/Matrix.cs
@@ -0,0 +1,84 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Z40
+{
+    internal class Matrix
+    {
+        public void RandomMatrix(int[,] matrix)
+        {
+            Random random = new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(0, 101);
+                }
+            }
+        }
+        public void PrintMatrix(int[,] matrix)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j],6}");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void SumMatrix(int[,] matrix1, int[,] matrix2)
+        {
+            if (matrix1.Length == matrix2.Length)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Результат сложения матриц:");
+                for (int i = 0; i < matrix1.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix1.GetLength(1); j++)
+                    {
+                        Console.Write($"{matrix1[i, j] + matrix2[i, j],4}");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        public static int[,] MulMatrix(int[,] matrix1, int[,] matrix2)
+        {
+            if (matrix1.GetLength(1) == matrix2.GetLength(0))
+            {
+                int[,] temp = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+                for (int i = 0; i < matrix1.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix2.GetLength(1); j++)
+                    {
+                        for (int k = 0; k < matrix2.GetLength(0); k++)
+                        {
+                            temp[i, j] += matrix1[i, k] * matrix2[k, j];
+                        }
+                    }
+                }
+                return temp;
+            }
+            else throw new Exception("Матрицы нельзя перемножить");
+        }
+        public int[,] TranspMatrix(int n, int m, int[,] matrix)
+        {
+            int[,] temp = new int[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    temp[i, j] = matrix[j, i];
+                }
+            }
+            return temp;
+        }
+    }
+}
---
+++ Zadanie40/Program.cs
@@ -0,0 +1,39 @@
+﻿using Z40;
+
+Console.WriteLine("Для матрицы N*M");
+Console.Write("Введите N: ");
+int n;
+int.TryParse(Console.ReadLine(), out n);
+Console.Write("Введите M: ");
+int m;
+int.TryParse(Console.ReadLine(), out m);
+int[,] matrixOne = new int[n, m];
+int[,] matrixTwo = new int[n, m];
+Matrix matrix = new Matrix();
+matrix.RandomMatrix(matrixOne);
+matrix.RandomMatrix(matrixTwo);
+matrix.PrintMatrix(matrixOne);
+matrix.PrintMatrix(matrixTwo);
+matrix.SumMatrix(matrixOne, matrixTwo);
+
+Console.WriteLine("\nРезультат умножения матриц:\n");
+try
+{
+    matrix.PrintMatrix(Matrix.MulMatrix(matrixOne, matrixTwo));
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+Console.WriteLine("\nТранспонированые матрицы:");
+try
+{
+    Console.WriteLine("\nПервая матрица:");
+    matrix.PrintMatrix(matrix.TranspMatrix(n, m, matrixOne));
+    Console.WriteLine("\nВторая матрица:");
+    matrix.PrintMatrix(matrix.TranspMatrix(n, m, matrixTwo));
+}
+catch (IndexOutOfRangeException e)
+{
+    Console.WriteLine(e.Message);
+}
